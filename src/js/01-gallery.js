3bc22ed10e2ed3c91ce6ee36dc15f402d1c0f57d
Andrii Slabkyi <aslabkyi@gmail.com>
2023-02-15 20:04:04 +0200

    Complete task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+let lightbox = null;
+
+galleryListEl.innerHTML = createGalleryList(galleryItems);
+galleryListEl.addEventListener('click', handleImgClick);
+
+function handleImgClick(e) {
+  e.preventDefault();
+
+  if (checkIfImg(e.target)) return;
+
+  createLightbox();
+}
+
+function createGalleryList(element) {
+  return element
+    .map(
+      ({ original, preview, description }) => `
+   <div class="gallery__item">
+   <a class="gallery__item" href=${original}>
+   <img class="gallery__image" src=${preview} alt=${description} />
+ </a>
+        </div>
+   `
+    )
+    .join('');
+}
+
+function checkIfImg(element) {
+  return element.nodeName !== 'IMG';
+}
+
+function createLightbox() {
+  lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+  });
+}
