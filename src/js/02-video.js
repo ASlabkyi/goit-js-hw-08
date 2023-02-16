6a8cd866b5d14073b5a685b63e56e338b5c34004
Andrii Slabkyi <aslabkyi@gmail.com>
2023-02-16 16:43:17 +0200

    Complete task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,11 +2,15 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const TIME_KEY = 'videoplayer-current-time';
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+function currentTime(data) {
+  localStorage.setItem(TIME_KEY, JSON.stringify(data.seconds));
+}
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+let time = JSON.parse(localStorage.getItem(TIME_KEY));
+if (time) {
+  player.setCurrentTime(time);
+}
+
+player.on('timeupdate', currentTime);
